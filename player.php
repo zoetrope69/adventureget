14f8eb39f5b1d5ca0c514e2c1939b0083314712a
Peter Jones <kephail@me.com>
2013-02-19 14:39:00 +0000

    added class template

--- player.php
+++ player.php
@@ -1,1 +1,20 @@
-//some bs in here
+<?php
+//The player class
+ 
+class Player { 
+    private $test;
+
+    function Player()
+    {
+    	$this->test = 'a test variable';
+    }
+    
+    public function aMemberFunc() { 
+        //print 'Inside `aMemberFunc()`'; 
+        echo $this->test;
+    } 
+} 
+
+$foo = new Player;
+$foo->aMemberFunc(); 
+?> 
