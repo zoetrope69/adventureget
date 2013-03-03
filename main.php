45ae1a0bf211bec25c9989ba14589e91035ccee9
Peter Jones <kephail@me.com>
2013-03-03 20:39:50 +0000

    I commented the wrong line..

--- main.php
+++ main.php
@@ -6,9 +6,9 @@ foreach (glob("classes/*.php") as $file)
     include $file;
 }
 include "functions.php";
-//include "parser2.php";
+include "parser2.php";
 $parser = new Parser();
-$parser = new Parser2();
+//$parser = new Parser2();
 
 
 if (isset($_POST['commands'])){
