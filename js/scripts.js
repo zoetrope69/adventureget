4266a284b321987140e608425e787bb110e510c0
Zac Colley <hi@zaccolley.com>
2013-03-10 14:19:24 +0000

    Core functionality transferred to JS... phew

--- js/scripts.js
+++ js/scripts.js
@@ -43,16 +43,18 @@ $('#commands').keydown(function(event) { // When keys are pressed in the input #
 
 // THE GUBBINS
 var game = new Game(commandListJSON, mapJSON);
-$data = game._areas[0][1].printDetails();
-$('#text').append($data); // Append on to the end of existing content
+var playerLocX = game._player.character.getLoc('x');
+var playerLocY = game._player.character.getLoc('y');
+var data = game._areas[playerLocY][playerLocX].printDetails();
+$('#text').append(data); // Append on to the end of existing content
 
 (function(){ // When document is done loading, load main.php
 })();
 
 function updateTerminal(commandsInput){
-	$commands = commandsInput.value;
-	$data = game._parser.parseCommands($commands);
- 	$('#text').append($data); // Append on to the end of existing content
+	var commands = commandsInput.value;
+	data = game._parser.parseCommands(commands);
+ 	$('#text').append(data); // Append on to the end of existing content
 	$('#commands').val("");	// Clear input box
 	/*if($data.indexOf("clearthatshit") !== -1){ // If returned output has clearscreen in it
 		$('#text').html(""); // Clear screen
