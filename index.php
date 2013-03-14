6f68e41af1ae9198cfc5a4a18ff0bd110ae0befe
Zac Colley <hi@zaccolley.com>
2013-03-14 03:27:14 +0000

    Got rid of some PHP

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
-<?php
-	$title = "adventureGet - super awesome text adventure game";
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title><?php echo $title; ?></title>
+	<title>adventureGet - super awesome text adventure game</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<header><h1><?php echo $title; ?><a href="http://www.github.com/zaccolley/adventureget" target="_blank">come help!</a></h1></header>
+	<header><h1>adventureGet - super awesome text adventure game<a href="http://www.github.com/zaccolley/adventureget" target="_blank">come help!</a></h1></header>
 	<article id="terminal">		
 		<section id="text"></section>
 		<label>&gt;</label><input id="commands" autofocus autocomplete="off" spellcheck="false"/>
